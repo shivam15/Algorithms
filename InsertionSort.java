880c063f272237307b411c7299a0f6560e278580
unknown <shivam.sarawagi68@gmail.com>
2018-11-21 17:47:03 +0530

    insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+/**
+ *
+ * @author Admin
+ */
+public class InsertionSort {
+
+    public static void main(String args[]) {
+        int[] arr = {1, 9, 5, 2, 3, 3};
+        printArray(insertionSort(arr));
+    }
+
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+}
