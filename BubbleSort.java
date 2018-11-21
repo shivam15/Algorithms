12790a258a92769fb81e6b8a4113bda386963400
unknown <shivam.sarawagi68@gmail.com>
2018-11-21 17:35:02 +0530

    bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+/**
+ *
+ * @author Admin
+ */
+public class BubbleSort {
+
+    public static void main(String args[]) {
+     int[] arr = {1,9,5,2,3};
+        printArray(bubbleSort(arr));
+    }
+    
+    public static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+}
