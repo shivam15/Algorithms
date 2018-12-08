b6fe8217f001bc6535433655a22b7a71712d21cb
shivam <shivam.sarawagi68@gmail.com>
2018-12-08 12:10:17 +0530

    dijsktra

--- Dijsktra.java
+++ Dijsktra.java
@@ -10,15 +10,21 @@ package Algorithms;
  * @author shivam
  */
 public class Dijsktra {
+
     static int V;
 
     public static void main(String args[]) {
-        int graph[][] = new int[][] {{0, 2, 0, 6, 0}, 
-                                    {2, 0, 3, 8, 5}, 
-                                    {0, 3, 0, 0, 7}, 
-                                    {6, 8, 0, 0, 9}, 
-                                    {0, 5, 7, 9, 0}};
-        V = 5;
+        int graph[][] = new int[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
+        {4, 0, 8, 0, 0, 0, 0, 11, 0},
+        {0, 8, 0, 7, 0, 4, 0, 0, 2},
+        {0, 0, 7, 0, 9, 14, 0, 0, 0},
+        {0, 0, 0, 9, 0, 10, 0, 0, 0},
+        {0, 0, 4, 14, 10, 0, 2, 0, 0},
+        {0, 0, 0, 0, 0, 2, 0, 1, 6},
+        {8, 11, 0, 0, 0, 0, 1, 0, 7},
+        {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        };
+        V = 9;
         dijsktraMST(graph);
     }
 
@@ -36,27 +42,29 @@ public class Dijsktra {
             int u = minKey(key, mst);
             mst[u] = true;
             for (int v = 0; v < V; v++) {
-                if (graph[u][v] != 0 && mst[v] == false && graph[u][v] < key[v]) {
+                if (graph[u][v] != 0 && mst[v] == false && key[u] + graph[u][v] < key[v]) {
                     parent[v] = u;
-                    key[v] = key[u]+graph[u][v];
+                    key[v] = key[u] + graph[u][v];
                 }
             }
         }
         printMST(key);
     }
-    static void printMST(int[] parent){
-        System.out.println("Vertex   Distance from Source"); 
-        for (int i = 0; i < V; i++) 
-            System.out.println(i+" \t \t "+parent[i]);
+
+    static void printMST(int[] parent) {
+        System.out.println("Vertex   Distance from Source");
+        for (int i = 0; i < V; i++) {
+            System.out.println(i + " \t \t " + parent[i]);
+        }
     }
+
     static int minKey(int[] key, boolean[] mst) {
         int min = Integer.MAX_VALUE;
         int min_index = -1;
-        for(int v=0;v<V;v++){
-            if (mst[v] == false && key[v] < min) 
-            { 
-                min = key[v]; 
-                min_index = v; 
+        for (int v = 0; v < V; v++) {
+            if (mst[v] == false && key[v] < min) {
+                min = key[v];
+                min_index = v;
             }
         }
         return min_index;
